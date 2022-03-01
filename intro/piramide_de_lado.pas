6a58988edcd599927190c679ef46122d929e3afa
jhonny <joao.manfron09@gmail.com>
2022-03-01 17:39:22 -0300

    exercicios piramide

--- intro/piramide_de_lado.pas
+++ intro/piramide_de_lado.pas
@@ -1,12 +1,12 @@
-program piramide;
+program piramide_de_lado;
 uses crt;
 var coluna, totalLinhas, linhaAtual : integer ;
 begin
 totalLinhas := 8;
     for linhaAtual := 0 to totalLinhas do
     begin
-        for coluna := 0 to totalLinhas * 2 do 
-            if (coluna <= totalLinhas - linhaAtual) or (coluna >= totalLinhas + linhaAtual) then
+        for coluna := totalLinhas - 4 downto 0 do 
+            if (coluna > totalLinhas - linhaAtual) or (linhaAtual < coluna)then
                 write ('.')
             else
                 write ('x');
