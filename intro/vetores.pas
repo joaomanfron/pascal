557b769aa3652c9b4efe830d342c1e9786431952
jhonny <joao.manfron09@gmail.com>
2022-03-14 17:00:56 -0300

    exercicios vetor

--- intro/vetores.pas
+++ intro/vetores.pas
@@ -1,11 +1,23 @@
 program vetores;
 uses crt;
 
+const
+RSIZE= 2;
+SSIZE= 4;
 var
-R : array [1..10] of integer;
-S : array [1..20] of integer;
+
+R : array [1..RSIZE] of integer;
+S : array [1..SSIZE] of integer;
+U : array of integer;
+i,n : integer;
 
 begin
-    for R := 1 to  10 do
-        read (R);
+    for i:= 1 to  2 do
+        read (R[i]);
+    for n:= 1 to  4 do
+        read (S[n]);
+    writeln;
+    for U := 1 to (RSIZE + SSIZE) do;
+        write (U)
+    
 end.
