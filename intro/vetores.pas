34f4afb4354d8489f3f30e14495abbfe2ff9217b
jhonny <joao.manfron09@gmail.com>
2022-03-17 17:05:58 -0300

    vetores

--- intro/vetores.pas
+++ intro/vetores.pas
@@ -2,31 +2,54 @@ program vetores;
 uses crt;
 
 const
-RSIZE= 2;
+RSIZE= 3;
 SSIZE= 4;
-USIZE= 6;
 var
 
 R : array [1..RSIZE] of integer;
 S : array [1..SSIZE] of integer;
-U : array [1..USIZE] of integer;
+U : array [1..(RSIZE+SSIZE)] of integer;
+C : array [1..RSIZE] of integer;
 i,n,j: integer;
+Usuario: string;
+elementoRepetido: boolean;
 
 begin
+    clrscr;
+    writeln ('insira seu nome');
+        read (Usuario);
+    writeln ('Ola ',Usuario, ' bemvindo ao programa de vetores, para começar, escreva os numeros do primeiro vetor com ',RSIZE,' casas');
     for i:= 1 to  RSIZE do
         read (R[i]);
+    writeln ('Muito bem ',Usuario, ' ,agora escreva o segundo vetor com ', SSIZE,' casas' );
     for n:= 1 to  SSIZE do
         read (S[n]);
     writeln;
+    writeln ('A junção dos vetores é a seguinte:');
     for j := 1 to (RSIZE + SSIZE) do
     begin
-        if j < 3 then      
+        if j <= RSIZE then      
             U[j] := R[j]
         else
             U[j] := S[j - RSIZE]
     end;
     for j := 1 to (RSIZE + SSIZE) do
         write (U[j],' ');
-    if U[j] = U[j] then
-        write (U[J]);
+    writeln;
+    writeln ('Os numeros dos vetores em comum são:');
+    elementoRepetido := false;
+    for i:= 1 to  RSIZE do
+        for n:= 1 to  SSIZE do
+            if R[i] = S[n] then
+            begin
+                elementoRepetido := true;
+                C[i] := S[n];
+
+            end;        
+    if elementoRepetido then
+        for i:= 1 to SSIZE do
+            writeln (C[i],' ')
+    else
+        writeln ('Não existem elementos repetidos');
+            
 end.
